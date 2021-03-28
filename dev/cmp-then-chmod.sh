579715f32d2fe15c3f4526d7454dec5105c5dfda
Ecat204528 <E.c.a.t.20.5.28@gmail.com>
2021-03-28 18:46:00 +0900

    add thre products and .gitignore.

--- dev/cmp-then-chmod.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-gcc $1 -o $2 && echo compilation completed successfully\n
-if(gcc $1)
-  chmod +x $2 && echo chmod completed successfully\n
--- finished-product/shscript/cmp-and-mv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Remove extension
-# example : foo.c -> foo
-filename=$1
-filename=${filename%.c}
-
-gcc $filename.c -o $filename.o
-mv $filename.o ~/workspace/work-c/finished-product/obj/
